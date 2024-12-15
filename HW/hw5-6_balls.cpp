f6d7c66865337f2a52f7116989b6c90e1c0cb8e3
kroften <sskozhemyakin2006@gmail.com>
2024-12-15 17:11:47 +0300

    Add juicy balls

--- HW/hw5-6_balls.cpp
+++ HW/hw5-6_balls.cpp
@@ -1,8 +1,38 @@
-import std;
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
 
-int main5_6() 
-{
-	
+void generate(int t);
 
-	return 0;
+
+int n = 0;
+vector<int> a = {};
+int counter = 0;
+
+int main5_6() {
+    cout << "Amount: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+        a.push_back(i);
+    generate(0);
+    cout << counter;
+
+    return 0;
+}
+
+void generate(int t) {
+    if (t == n) {
+        for (int i = 0; i < n; i++) {
+            if (a[i] == i + 1) {
+                counter++;
+                return;
+            }
+        }
+    }
+    for (int j = t; j < n; j++) {
+        swap(a[t], a[j]);
+        generate(t + 1);
+        swap(a[t], a[j]);
+    }
 }
